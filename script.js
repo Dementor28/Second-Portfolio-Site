b2eae4cf4878d1326db078eb8e278fc35aaa6448
Abdullah <abdullah1@myseneca.ca>
2023-09-22 05:06:46 -0400

    Poilishing the site

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function toggleMenu() {
     });
   });
   
+  var backToTopButton = document.querySelector('.back-to-top');
+
+  window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 100) {
+          // Show the button when the user scrolls down 100px
+          backToTopButton.style.display = 'block';
+      } else {
+          // Hide the button when the user is at the top
+          backToTopButton.style.display = 'none';
+      }
+  });
+
+  backToTopButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      // Scroll to the top of the page smoothly
+      window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+      });
+  });
