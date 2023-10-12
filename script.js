3e06887376eb320441179f67b9fe89976a0f3ad0
Abdullah <abdullah1@myseneca.ca>
2023-10-12 12:42:10 -0400

    Changes to Website Design, Updated Resume, minor code tweaks

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function toggleMenu() {
     });
   });
   
-  var backToTopButton = document.querySelector('.back-to-top');
+  var backToTopButton = document.querySelector('.to-top');
 
   window.addEventListener('scroll', function () {
-      if (window.pageYOffset > 100) {
+      if (window.pageYOffset > 1000) {
           // Show the button when the user scrolls down 100px
           backToTopButton.style.display = 'block';
       } else {
@@ -34,4 +34,20 @@ function toggleMenu() {
           top: 0,
           behavior: 'smooth'
       });
+  });
+
+  // Adding Smooth scrolling behavior to navigation buttons
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: 'smooth'
+        });
+      }
+    });
   });
